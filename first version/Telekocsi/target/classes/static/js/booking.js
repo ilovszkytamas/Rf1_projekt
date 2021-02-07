88292e21aa183a0e32bd5e8d90d04a445ac92b10
Ilovszky Tamás <h659385@stud.u-szeged.hu>
2021-02-07 23:24:41 +0100

    final commit

---
+++ first version/Telekocsi/target/classes/static/js/booking.js
@@ -0,0 +1,79 @@
+$('document').ready(function() {
+    if (document.cookie.indexOf("userid") == -1) {
+        $(location).attr('href', window.location.protocol + '//' + window.location.host + "/login");
+    }
+
+    $("#logout").click(function () {
+        Cookies.remove("userid");
+        location.reload();
+    })
+
+    $("#send").click(function () {
+        var from = $("#from").val();
+        var to = $("#to").val();
+        var directions = from+";"+to;
+        $.ajax({
+            url: window.location.protocol + '//' + window.location.host +'/booking/getRides?from='+from+"&to="+to,
+            type: 'get',
+            dataType:'json',
+            success: function (datas) {
+                console.log(datas);
+                var rides = "";
+                rides +=("<tr>");
+                rides +=("<th>Azonosító</th>");
+                rides +=("<th>Rendszám</th>");
+                rides +=("<th>Felhasználónév</th>");
+                rides +=("<th>Kiindulás hely</th>");
+                rides +=("<th>Érkezési hely</th>");
+                rides +=("<th>Indulási idő</th>");
+                rides +=("<th>Érkezés idő</th>");
+                rides +=("<th>Ár</th>");
+                rides +=("<th>Férőhely</th>");
+                rides +=("</tr>");
+                $("#ridelist").append(rides);
+                for(var i = 0; i <= datas.length; i++) {
+                    rides = "";
+                    var data = JSON.parse(datas[i]);
+                    rides +=("<tr>");
+                    rides +=("<td>" + data.id + "</td>");
+                    rides +=("<td>" + data.car.plate_number + "</td>");
+                    rides +=("<td>" + data.car.user.username + "</td>");
+                    rides +=("<td>" + data.departure + "</td>");
+                    rides +=("<td>" + data.arrival + "</td>");
+                    var x = new Date(data.departuretime);
+                    var y = new Date(data.arrivaltime);
+                    var departuretime = x.getFullYear() + "-" + (x.getMonth() + 1) + "-" + x.getDate() + " " + x.getHours() + ":" + x.getMinutes();
+                    var arrivaltime = y.getFullYear() + "-" + (y.getMonth() + 1) + "-" + y.getDate() + " " + y.getHours() + ":" + y.getMinutes();
+                    rides +=("<td>" + departuretime + "</td>");
+                    rides +=("<td>" + arrivaltime + "</td>");
+                    rides +=("<td>" + data.price + "</td>");
+                    rides +=("<td>" + data.car.seats + "</td>");
+                    rides +=('<td><button class="choice">Foglalás</button></td>');
+                    rides +=("</tr>");
+                    $("#ridelist").append(rides);
+                }
+                $("#ridelist").append("</table>");
+            },
+            error: function (data) {
+                console.log("hiba", data.responseText);
+            }
+        });
+    })
+
+    $('#ridelist').on('click','.choice', function () {
+        var currentrow = $(this).parent().parent();
+        var rideid = currentrow.find('td:eq(0)').text();
+        $.ajax({
+            url: window.location.protocol + '//' + window.location.host +'/booking/bookRide?rideid='+rideid+"&userid="+Cookies.get("userid"),
+            type:'get',
+            contentType: 'application/json',
+            dataType:'text',
+            success:function(data){
+                console.log(data);
+            },
+            error: function (data) {
+                console.log("hiba",data.responseText);
+            }
+        })
+    });
+});
---
+++ first version/Telekocsi/target/classes/static/js/create.js
@@ -0,0 +1,52 @@
+$('document').ready(function() {
+    if (document.cookie.indexOf("userid") == -1) {
+        $(location).attr('href', window.location.protocol + '//' + window.location.host + "/login");
+    }
+
+    $("#logout").click(function () {
+        Cookies.remove("userid");
+        location.reload();
+    })
+
+    $.ajax({
+        url: window.location.protocol + '//' + window.location.host +'/profile/getCars?id=' + Cookies.get("userid"),
+        type: 'get',
+        success: function (datas) {
+            for (var i = 0; i <= datas.length; i++) {
+                var data = JSON.parse(datas[i]);
+                $("#cars").append("<option>" + data.plate_number + "</option>");
+            }
+            $("#cars").append("</select>");
+        },
+        error: function (data) {
+            console.log("hiba", data.responseText);
+        }
+    });
+
+    $("#sendRide").click(function () {
+        var arrival = $('#arrival').val();
+        var departure = $('#departure').val();
+        var price = $('#price').val();
+        var departuretime = $('#departuretime').val();
+        var arrivaltime = $('#arrivaltime').val();
+
+        var selectedcar = $("#cars option:selected").text();
+
+        var rideData = departure +";"+arrival+";"+departuretime+";"+arrivaltime+";"+price+";"+selectedcar;
+        console.log(rideData);
+
+        $.ajax({
+            url: window.location.protocol + '//' + window.location.host +'/create/addRide',
+            type: 'post',
+            contentType: 'application/json',
+            dataType: 'text',
+            data: rideData,
+            success: function (data) {
+                console.log(data);
+            },
+            error: function (data) {
+                console.log("hiba", data.responseText);
+            }
+        });
+    });
+});
